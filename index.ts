8e50c720eda45bd863fde9a3343eadda3684f428
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-09-04 03:00:51 +0900

    upload

---
+++ index.ts
@@ -0,0 +1,21 @@
+import "jsr:@std/dotenv/load";
+import { Client } from "npm:discord.js";
+
+const env = Deno.env.toObject();
+
+const client = new Client({
+    "intents": 8
+});
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on("messageCreate", (message) => {
+    console.log(message.content);
+    if (message.content === "!ping") {
+        message.reply("Pong!");
+    }
+});
+
+client.login(env["BOT_TOKEN"]);
